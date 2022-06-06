c2d259188326da54784097992c5b72093eccaf68
kish-00 <59705198+kish-00@users.noreply.github.com>
2022-06-06 11:49:59 +0300

    minor changes

--- src/main/java/com/stock/market/StockMarket/StockMarketApplication.java
+++ src/main/java/com/stock/market/StockMarket/StockMarketApplication.java
@@ -1,13 +1,13 @@
 package com.stock.market.StockMarket;
 
+import com.stock.market.StockMarket.Controller.StockController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StockMarketApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StockMarketApplication.class, args);
 	}
-
 }
